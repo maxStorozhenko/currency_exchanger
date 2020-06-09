6d80169b8b6d8b5c9d2b1479c286644b2c0062ec
Maxim <maxistorozhenko@gmail.com>
2020-06-09 10:08:18 +0300

    crispy forms, edit/delete rate for superuser

--- src/settings/settings.py
+++ src/settings/settings.py
@@ -2,6 +2,8 @@ import os
 
 from celery.schedules import crontab
 
+from django.urls import reverse_lazy
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = 'fp_grwa77&d75%01dziwslea2*t*(fmas6rrbyg+nbr%k$$wr8'
@@ -20,6 +22,7 @@ INSTALLED_APPS = [
 
     'debug_toolbar',
     'django_extensions',
+    'crispy_forms',
 
     'account',
     'rate',
@@ -111,3 +114,17 @@ CELERY_BEAT_SCHEDULE = {
         'schedule': crontab(minute='*/15')
     }
 }
+
+LOGIN_REDIRECT_URL = reverse_lazy('index')
+LOGOUT_REDIRECT_URL = reverse_lazy('index')
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'testdjangohillel@gmail.com'
+EMAIL_HOST_PASSWORD = 'hilleldjango2020'
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
