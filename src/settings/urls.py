e1b7e3823c4c63da3ed6c909ddc92d382ede4544
Maxim <maxistorozhenko@gmail.com>
2020-07-13 19:49:58 +0300

    auth views

--- src/settings/urls.py
+++ src/settings/urls.py
@@ -1,8 +1,6 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth.views import PasswordResetCompleteView, PasswordResetConfirmView, PasswordResetDoneView, \
-    PasswordResetView
 from django.urls import include, path
 from django.views.generic import TemplateView
 
