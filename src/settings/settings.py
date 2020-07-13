749257e8f8c53a067b24a1f3d46473fc471ca319
Maxim <maxistorozhenko@gmail.com>
2020-07-13 19:47:22 +0300

    auth views

--- src/settings/settings.py
+++ src/settings/settings.py
@@ -47,7 +47,8 @@ ROOT_URLCONF = 'settings.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),
+                 os.path.join(BASE_DIR, 'account', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
