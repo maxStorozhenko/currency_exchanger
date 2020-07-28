ca38e1a7f7e0b7c221d16e178c2275371db0b4b1
Maxim <maxistorozhenko@gmail.com>
2020-07-28 17:25:57 +0300

    Pagination + filters

--- src/account/api/views.py
+++ src/account/api/views.py
@@ -1,12 +1,16 @@
 from account.api.serializers import AccountSerializer, ContactSerializer
 from account.models import Contact, User
 
+from django_filters.rest_framework import DjangoFilterBackend
+
 from rest_framework import generics
 
 
 class UserListCreateView(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = AccountSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['username', 'email']
 
 
 class UserReadUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
